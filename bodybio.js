2f9360b81ea48698eee6f0541fefbc039a899cd4
The5thTear <144561947+The5thTear@users.noreply.github.com>
2024-01-12 14:15:08 -0700

    update bodybio.js

--- bodybio.js
+++ bodybio.js
@@ -1,56 +1,78 @@
-let img1, img2;
+let img;
 let particles = [];
-let performanceLevel = 'high';
-let particleCount;
+let attractionPoints = [];
 
 function preload() {
-    img1 = loadImage('assets/images/andrewsim1.png');
-    img2 = loadImage('assets/images/andrewsim2.png');
+    // Preload the image
+    img = loadImage('assets/images/andrewsim1.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    switch(performanceLevel) {
-        case 'low': particleCount = 100; break;
-        case 'medium': particleCount = 500; break;
-        case 'high': particleCount = 1000; break;
-    }
-    for (let i = 0; i < particleCount; i++) {
-        particles.push(new Particle());
-    }
-}
-
-function draw() {
-    background(0);
-    let img = (frameCount % 300 < 150) ? img1 : img2;
+    // Call the processImage function when setup runs
+    img.loadPixels();
+    processImage();
+    img.updatePixels();
+  }
+  
+  function draw() {
+    // Render the processed image
+    background(255);
     image(img, 0, 0, width, height);
-    particles.forEach(particle => {
-        particle.update();
-        particle.show();
-    });
-}
+  }
+  
+  function processImage() {
+    for (let i = 0; i < img.pixels.length; i += 4) {
+      let r = img.pixels[i];
+      let g = img.pixels[i + 1];
+      let b = img.pixels[i + 2];
+  
+      // Calculate brightness manually
+      let brightness = (r + g + b) / 3;
+      
+      if (brightness < 50) {
+        // Adjust color based on brightness
+        img.pixels[i] = 255 - brightness; // Red channel
+        img.pixels[i + 1] = 0; // Green channel
+        img.pixels[i + 2] = brightness; // Blue channel
+      }
+    }
+  }
 
 class Particle {
-    constructor() {
-        this.pos = createVector(random(width), random(height));
+    constructor(x, y) {
+        this.pos = createVector(x, y);
         this.vel = createVector();
         this.acc = createVector();
     }
+
+    attractedToClosest(points) {
+        // Find the closest point of attraction
+        let closestPoint;
+        let recordDistance = Infinity;
+        for (let point of points) {
+            let d = p5.Vector.dist(this.pos, point);
+            if (d < recordDistance) {
+                recordDistance = d;
+                closestPoint = point;
+            }
+        }
+
+        // Calculate the attraction force towards that point
+        let force = p5.Vector.sub(closestPoint, this.pos);
+        let strength = 5 / (force.mag() + 1); // Example force calculation
+        force.setMag(strength);
+        this.acc.add(force);
+    }
+
     update() {
-        let target = createVector(mouseX, mouseY);
-        this.acc = p5.Vector.sub(target, this.pos);
-        this.acc.setMag(0.1);
         this.vel.add(this.acc);
         this.pos.add(this.vel);
-        this.vel.limit(5);
+        this.acc.mult(0); // Reset acceleration
     }
-    show() {
-        noStroke();
-        fill(255, 150, 0, 200); // Orange color with some transparency
-        circle(this.pos.x, this.pos.y, 4); // Draw the particle
-    }
-}
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    show() {
+        stroke(0);
+        point(this.pos.x, this.pos.y);
+    }
 }
