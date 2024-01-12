34f08b2984f6aa6bcd2f229bac41bb32019bab2d
The5thTear <144561947+The5thTear@users.noreply.github.com>
2024-01-11 18:12:08 -0700

    fix about me page

--- script.js
+++ script.js
@@ -11,13 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.getElementById('question-mark').addEventListener('click', function() {
     var aboutMe = document.getElementById('about-me');
-    if (aboutMe.style.visibility === 'visible') {
-        aboutMe.style.visibility = 'hidden';
-        aboutMe.style.width = '0';
-        this.style.right = '10px';
-    } else {
+    if (aboutMe.style.visibility === 'hidden') {
         aboutMe.style.visibility = 'visible';
-        aboutMe.style.width = '300px'; // Adjust as needed
-        this.style.right = '300px'; // Adjust as needed
+        aboutMe.style.opacity = '1';
+    } else {
+        aboutMe.style.visibility = 'hidden';
+        aboutMe.style.opacity = '0';
     }
 });
