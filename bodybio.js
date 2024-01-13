cd2ba1db853f40fd0100a5b78d459ea09a389afa
The5thTear <144561947+The5thTear@users.noreply.github.com>
2024-01-12 18:46:10 -0700

    please fix this

--- bodybio.js
+++ bodybio.js
@@ -1,99 +1,73 @@
 let img;
 let particles = [];
-let attractionPoints = [];
+const particleCount = 500; // Reduced number of particles
 
 function preload() {
-    // Preload the image
     img = loadImage('assets/images/andrewsim1.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     img.loadPixels();
-    processImage();
-    img.updatePixels();
 
     // Initialize particles
-    for (let i = 0; i < 1000; i++) { // You can adjust the number of particles
+    for (let i = 0; i < particleCount; i++) {
         particles.push(new Particle(random(width), random(height)));
     }
 }
-  
+
 function draw() {
     background(255);
     image(img, 0, 0, width, height);
 
     // Update and display particles
     for (let p of particles) {
-        p.attractedTo(img); // Attract particles to darker areas
+        p.attractedTo(img);
         p.update();
         p.show();
     }
 }
-  
-  function processImage() {
-    for (let i = 0; i < img.pixels.length; i += 4) {
-      let r = img.pixels[i];
-      let g = img.pixels[i + 1];
-      let b = img.pixels[i + 2];
-  
-      // Calculate brightness manually
-      let brightness = (r + g + b) / 3;
-      
-      if (brightness < 50) {
-        // Adjust color based on brightness
-        img.pixels[i] = 255 - brightness; // Red channel
-        img.pixels[i + 1] = 0; // Green channel
-        img.pixels[i + 2] = brightness; // Blue channel
-      }
-    }
-  }
 
 class Particle {
     constructor(x, y) {
         this.pos = createVector(x, y);
         this.vel = createVector();
         this.acc = createVector();
+        this.maxSpeed = 5; // Limiting the maximum speed
     }
 
     attractedTo(img) {
-        // Get the image pixels
-        img.loadPixels();
         let record = Infinity;
         let closestPixel;
-    
-        // Iterate over a subset of pixels for performance
-        for (let y = 0; y < img.height; y += 10) {
-            for (let x = 0; x < img.width; x += 10) {
+
+        // Sample a subset of pixels
+        for (let y = 0; y < img.height; y += 20) {
+            for (let x = 0; x < img.width; x += 20) {
                 let index = (x + y * img.width) * 4;
-                let r = img.pixels[index];
-                let g = img.pixels[index + 1];
-                let b = img.pixels[index + 2];
-                let brightness = (r + g + b) / 3;
-    
-                // Check if this pixel is darker than the current record
+                let brightness = (img.pixels[index] + img.pixels[index + 1] + img.pixels[index + 2]) / 3;
+
                 if (brightness < record) {
                     record = brightness;
                     closestPixel = createVector(x, y);
                 }
             }
         }
-    
-        // Create a force vector towards the closest dark pixel
+
         let force = p5.Vector.sub(closestPixel, this.pos);
-        force.setMag(0.1); // You can adjust the strength
+        force.setMag(0.1);
         this.acc.add(force);
     }
 
     update() {
         this.vel.add(this.acc);
+        this.vel.limit(this.maxSpeed); // Limit the speed
         this.pos.add(this.vel);
-        this.acc.mult(0); // Reset acceleration
+        this.acc.mult(0);
     }
 
     show() {
-        fill(255); // White color
+        fill(255);
         noStroke();
-        ellipse(this.pos.x, this.pos.y, 2, 2); // Small circle for each particle
+        ellipse(this.pos.x, this.pos.y, 2, 2);
     }
 }
